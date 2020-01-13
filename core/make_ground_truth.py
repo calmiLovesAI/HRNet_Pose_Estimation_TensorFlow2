b33827099397c54a92c845af792be3de4651c025
calmisential <xinyu_std@163.com>
2020-01-13 10:19:04 +0800

    Update the code of image and keypoints transform

--- core/make_ground_truth.py
+++ core/make_ground_truth.py
@@ -1,6 +1,6 @@
 import tensorflow as tf
 import numpy as np
-from utils.tools import read_image, random_crop_and_resize_image, point_in_rect
+from utils.transforms import read_image, RandomCropTransform
 
 
 class GroundTruth(object):
@@ -55,6 +55,7 @@ class GroundTruth(object):
         keypoints_tensor = tf.convert_to_tensor(value=keypoints, dtype=tf.dtypes.float32)
         keypoints_tensor = tf.reshape(keypoints_tensor, shape=(-1, 3))
 
+        # Resize the image, and change the coordinates of the keypoints accordingly.
         image_tensor, keypoints_tensor = self.__image_and_keypoints_process(image_file, keypoints_tensor, bbox)
 
         keypoints_3d, keypoints_3d_exist = self.__get_keypoints_3d(keypoints_tensor)
@@ -62,29 +63,10 @@ class GroundTruth(object):
 
     def __image_and_keypoints_process(self, image_dir, keypoints, bbox):
         image_tensor = read_image(image_dir, self.config_params)
-        resized_image, resize_ratio, crop_rect = random_crop_and_resize_image(image_tensor, bbox, self.image_size[0], self.image_size[1])
-        # Change the keypoints according to the resize_ratio and crop_rect.
-        keypoints = self.__change_keypoints(resize_ratio, crop_rect, keypoints)
+        transform = RandomCropTransform(image=image_tensor, keypoints=keypoints, bbox=bbox, resize_h=self.image_size[0], resize_w=self.image_size[1], num_of_joints=self.num_of_joints)
+        resized_image, resize_ratio, crop_rect = transform.image_transform()
+        keypoints = transform.keypoints_transform(resize_ratio, crop_rect)
         return resized_image, keypoints
-
-    def __change_keypoints(self, resize_ratio, crop_rect, keypoints):
-        crop_rect = crop_rect.numpy()
-        keypoints = keypoints.numpy()
-        # First determine whether the point is inside the crop area.
-        for i in range(self.num_of_joints):
-            if not point_in_rect(point_x=keypoints[i, 0], point_y=keypoints[i, 1], rect=crop_rect):
-                keypoints[i, 2] = 0.0
-
-        for i in range(self.num_of_joints):
-            if keypoints[i, 2] > 0.0:
-                # Calculate the coordinates of the keypoints after cropping the original picture.
-                keypoints[i, 0] = keypoints[i, 0] - crop_rect[0]
-                keypoints[i, 1] = keypoints[i, 1] - crop_rect[1]
-                # Calculate the coordinates of the keypoints after resizing.
-                keypoints[i, 0] = int(keypoints[i, 0] * resize_ratio)
-                keypoints[i, 1] = int(keypoints[i, 1] * resize_ratio)
-        keypoints = tf.convert_to_tensor(value=keypoints, dtype=tf.dtypes.float32)
-        return keypoints
 
     def __get_keypoints_3d(self, keypoints_tensor):
         keypoints_3d_list = []
---
+++ utils/transforms.py
@@ -0,0 +1,94 @@
+import tensorflow as tf
+
+
+def read_image(image_dir, cfg):
+    image_content = tf.io.read_file(filename=image_dir)
+    # The 'image' has been normalized.
+    image = tf.io.decode_image(contents=image_content, channels=cfg.CHANNELS, dtype=tf.dtypes.float32)
+    return image
+
+
+# Determine whether a point is within a rectangular border.
+def point_in_rect(point_x, point_y, rect):
+    # rect : (x, y, w, h)
+    xmin = rect[0]
+    ymin = rect[1]
+    xmax = xmin + rect[2]
+    ymax = ymin + rect[3]
+    if xmin <= point_x <= xmax and ymin <= point_y <= ymax:
+        is_point_in_rect = True
+    else:
+        is_point_in_rect = False
+    return is_point_in_rect
+
+
+class RandomCropTransform(object):
+    def __init__(self, image, keypoints, bbox, resize_h, resize_w, num_of_joints):
+        self.image = image
+        self.keypoints = keypoints
+        self.bbox = bbox
+        self.resize_h = resize_h
+        self.resize_w = resize_w
+        self.num_of_joints = num_of_joints
+
+    def image_transform(self):
+        if self.resize_h != self.resize_w:
+            raise ValueError("The values of resize_h and resize_w should be equal.")
+        human_instance = tf.image.crop_to_bounding_box(image=self.image,
+                                                       offset_height=self.bbox[1],
+                                                       offset_width=self.bbox[0],
+                                                       target_height=self.bbox[3],
+                                                       target_width=self.bbox[2])
+        left_top_of_human_instance = self.bbox[0:2]
+        crop_rect, cropped_image = self.__random_crop_in_roi(image=self.image, roi=human_instance,
+                                                             left_top_of_roi=left_top_of_human_instance)
+        resize_ratio = self.resize_h / crop_rect.shape[-1]
+        resized_image = tf.image.resize(images=cropped_image, size=[self.resize_h, self.resize_w])
+        return resized_image, resize_ratio, crop_rect
+
+    def keypoints_transform(self, resize_ratio, crop_rect):
+        crop_rect = crop_rect.numpy()
+        transformed_keypoints = self.keypoints.numpy()
+        # First determine whether the point is inside the crop area.
+        for i in range(self.num_of_joints):
+            if not point_in_rect(point_x=transformed_keypoints[i, 0], point_y=transformed_keypoints[i, 1], rect=crop_rect):
+                transformed_keypoints[i, 2] = 0.0
+
+        for i in range(self.num_of_joints):
+            if transformed_keypoints[i, 2] > 0.0:
+                # Calculate the coordinates of the keypoints after cropping the original picture.
+                transformed_keypoints[i, 0] = transformed_keypoints[i, 0] - crop_rect[0]
+                transformed_keypoints[i, 1] = transformed_keypoints[i, 1] - crop_rect[1]
+                # Calculate the coordinates of the keypoints after resizing.
+                transformed_keypoints[i, 0] = int(transformed_keypoints[i, 0] * resize_ratio)
+                transformed_keypoints[i, 1] = int(transformed_keypoints[i, 1] * resize_ratio)
+        transformed_keypoints = tf.convert_to_tensor(value=transformed_keypoints, dtype=tf.dtypes.float32)
+        return transformed_keypoints
+
+    def __random_crop_in_roi(self, image, roi, left_top_of_roi):
+        roi_h = roi.shape[0]
+        roi_w = roi.shape[1]
+        if roi_h > roi_w:
+            longer_border = roi_h
+            shorter_border = roi_w
+        else:
+            longer_border = roi_w
+            shorter_border = roi_h
+        random_coord = tf.random.uniform(shape=(), minval=0, maxval=longer_border - shorter_border)
+        if longer_border == roi_h:
+            x_random_crop = left_top_of_roi[0]
+            y_random_crop = int(left_top_of_roi[1] + random_coord)
+        else:
+            x_random_crop = int(left_top_of_roi[0] + random_coord)
+            y_random_crop = left_top_of_roi[1]
+        crop_rect = tf.convert_to_tensor(value=[x_random_crop, y_random_crop, shorter_border, shorter_border],
+                                         dtype=tf.dtypes.int32)
+        cropped_image = tf.image.crop_to_bounding_box(image=image,
+                                                      offset_height=y_random_crop,
+                                                      offset_width=x_random_crop,
+                                                      target_height=shorter_border,
+                                                      target_width=shorter_border)
+        return crop_rect, cropped_image
+
+    def keypoints_to_original(self):
+        raise NotImplementedError("The method has not been implemented.")
