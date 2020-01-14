e8935ebd9fbae36f44e54ff97834a0ddb1437b8d
calmisential <xinyu_std@163.com>
2020-01-14 15:29:29 +0800

    Fix bug in inference

--- utils/work_flow.py
+++ utils/work_flow.py
@@ -20,7 +20,7 @@ def print_model_summary(network):
 
 def get_max_preds(heatmap_tensor):
     heatmap = heatmap_tensor.numpy()
-    batch_size, height, width, num_of_joints = heatmap.shape[0], heatmap.shape[1], heatmap.shape[2], heatmap.shape[-1]
+    batch_size, _, width, num_of_joints = heatmap.shape[0], heatmap.shape[1], heatmap.shape[2], heatmap.shape[-1]
     heatmap = heatmap.reshape((batch_size, -1, num_of_joints))
     index = np.argmax(heatmap, axis=1)
     maxval = np.amax(heatmap, axis=1)
@@ -28,8 +28,10 @@ def get_max_preds(heatmap_tensor):
     maxval = maxval.reshape((batch_size, 1, num_of_joints))
     preds = np.tile(index, (1, 2, 1)).astype(np.float32)
 
-    preds[:, 0, :] = (preds[:, 0, :]) / width
-    preds[:, 1, :] = np.floor((preds[:, 1, :]) / height)
+    # preds[:, 0, :] = (preds[:, 0, :]) / width
+    # preds[:, 1, :] = np.floor((preds[:, 1, :]) / height)
+    preds[:, 0, :] = np.ceil((preds[:, 0, :]) / width)
+    preds[:, 1, :] = preds[:, 1, :] % width
 
     pred_mask = np.tile(np.greater(maxval, 0.0), (1, 2, 1))
     pred_mask = pred_mask.astype(np.float32)
